a3b91bd311e8eb58ec52559a2e1d57236af10fac
Martin Richard Adam <g3adam@cdf.toronto.edu>
2016-03-28 17:14:01 -0400

    minor changes to rubiks/Piece.h

--- Piece.h
+++ Piece.h
@@ -1,6 +1,19 @@
-class Piece{
-    public: 
-        Piece();
-        Piece(Colour col);
+#ifndef RUBIKS_PIECE_H_INCLUDED
+#define RUBIKS_PIECE_H_INCLUDED
+
+#include "Colour.h"
+
+class Face{
+    public:
+        Face();
         
 }
+
+class Piece{
+    public: 
+        Piece(std::vector<Face> faces);
+        Piece(std::vector<Colour> cols);
+        draw();
+}
+
+#endif
